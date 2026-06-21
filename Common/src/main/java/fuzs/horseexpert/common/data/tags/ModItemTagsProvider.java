82f336b57137f506ec49997775146b34965fd48f
Fuzss <28218241+Fuzss@users.noreply.github.com>
2026-06-21 12:37:57 +0200

    fix crash without trinkets

--- Common/src/main/java/fuzs/horseexpert/common/data/tags/ModItemTagsProvider.java
+++ Common/src/main/java/fuzs/horseexpert/common/data/tags/ModItemTagsProvider.java
@@ -2,6 +2,7 @@ package fuzs.horseexpert.common.data.tags;
 
 import eu.pb4.trinkets.api.DefaultTrinketSlotTags;
 import fuzs.horseexpert.common.init.ModRegistry;
+import fuzs.puzzleslib.common.api.core.v1.ModLoaderEnvironment;
 import fuzs.puzzleslib.common.api.data.v2.core.DataProviderContext;
 import fuzs.puzzleslib.common.api.data.v2.tags.AbstractTagProvider;
 import net.minecraft.core.HolderLookup;
@@ -17,6 +18,8 @@ public class ModItemTagsProvider extends AbstractTagProvider<Item> {
     @Override
     public void addTags(HolderLookup.Provider provider) {
         this.tag(ModRegistry.INSPECTION_EQUIPMENT_ITEM_TAG).add(ModRegistry.MONOCLE_ITEM);
-        this.tag(DefaultTrinketSlotTags.HEAD_FACE).addTag(ModRegistry.INSPECTION_EQUIPMENT_ITEM_TAG);
+        if (ModLoaderEnvironment.INSTANCE.isModLoaded("trinkets_updated")) {
+            this.tag(DefaultTrinketSlotTags.HEAD_FACE).addTag(ModRegistry.INSPECTION_EQUIPMENT_ITEM_TAG);
+        }
     }
 }
